4f5cf50420a3cd5d69f516c0ea1e4f221659f367
elmo <elmooordista.com>
2023-05-04 14:35:53 +0800

    feat new

--- resources/js/mixins/firebaseHelper.vue
+++ resources/js/mixins/firebaseHelper.vue
@@ -3,18 +3,18 @@ import { getMessaging, onMessage, getToken } from 'firebase/messaging'
 import { messaging } from '../plugins/firebase'
 export default {
   methods: {
-    async getDeviceToken () {
-      try {
-          const token = await getToken(messaging, {
-            vapidKey: 'BLmcPdKYUAajYCxQC1DPNvfpewEqKm2vmnv9NL-T8n37GOscYjCzh1txMWfQ_HAi0p0wIcPqbG7w3H_QIaGMMbw'
-          })
-          if (token) {
-            this.initialize(token)
-          }
-        } catch (err) {
-            console.log('An error occurred while retrieving token. ', err);
-        }
-    },
+    // async getDeviceToken () {
+    //   try {
+    //       const token = await getToken(messaging, {
+    //         vapidKey: 'BLmcPdKYUAajYCxQC1DPNvfpewEqKm2vmnv9NL-T8n37GOscYjCzh1txMWfQ_HAi0p0wIcPqbG7w3H_QIaGMMbw'
+    //       })
+    //       if (token) {
+    //         this.initialize(token)
+    //       }
+    //     } catch (err) {
+    //         console.log('An error occurred while retrieving token. ', err);
+    //     }
+    // },
     activateNotification(){
       const messaging = getMessaging()
       onMessage(messaging, (payload) => {
